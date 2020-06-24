ea3a2297a87f057f7371e1ebe8a71d788471d13e
Vivian Han <qintonghan@google.com>
2020-06-24 21:00:53 +0000

    Move functions to next pull request.

--- src/test/java/com/googleintern/wfm/ruleengine/GroupingUtilTest.java
+++ src/test/java/com/googleintern/wfm/ruleengine/GroupingUtilTest.java
@@ -3,9 +3,12 @@ package src.test.java.com.googleintern.wfm.ruleengine;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import com.google.common.collect.ImmutableSet;
 >>>>>>> c6e7411... WIP for code review comments.
+=======
+>>>>>>> f6ba02b... Move functions to next pull request.
 import com.google.common.collect.ImmutableSetMultimap;
 import com.opencsv.exceptions.CsvException;
 import org.junit.Assert;
@@ -16,14 +19,18 @@ import src.main.java.com.googleintern.wfm.ruleengine.action.WorkgroupIdGroupingU
 import src.main.java.com.googleintern.wfm.ruleengine.model.FilterModel;
 import src.main.java.com.googleintern.wfm.ruleengine.model.PoolAssignmentModel;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import src.main.java.com.googleintern.wfm.ruleengine.model.RuleModel;
 >>>>>>> c6e7411... WIP for code review comments.
+=======
+>>>>>>> f6ba02b... Move functions to next pull request.
 import src.main.java.com.googleintern.wfm.ruleengine.model.UserPoolAssignmentModel;
 
 import java.io.IOException;
 
 /**
+<<<<<<< HEAD
 <<<<<<< HEAD
  * GroupingUtilTest class is used to test the functionality of both WorkgroupIdGroupingUtil and
  * CasePoolIdAndPermissionIdGroupingUtil classes.
@@ -31,6 +38,10 @@ import java.io.IOException;
  * GroupingTest class is used to test the functionality of both WorkgroupIdGroupingUtil and
  * GroupByCasePoolIdAndPermissionId.
 >>>>>>> c6e7411... WIP for code review comments.
+=======
+ * GroupingUtilTest class is used to test the functionality of both WorkgroupIdGroupingUtil and
+ * CasePoolIdAndPermissionIdGroupingUtil classes.
+>>>>>>> f6ba02b... Move functions to next pull request.
  */
 public class GroupingUtilTest {
   private static final String TEST_CSV_FILE_PATH =
@@ -40,48 +51,6 @@ public class GroupingUtilTest {
   private static final int EXPECTED_WORKGROUP_ID_NUMBER = 2;
   private static final Long EXPECTED_FIRST_WORKGROUP_ID = 1122L;
   private static final Long EXPECTED_SECOND_WORKGROUP_ID = 2233L;
-<<<<<<< HEAD
-=======
-  private static final ImmutableList<ImmutableSet<FilterModel>> emptyFilters =
-      ImmutableList.<ImmutableSet<FilterModel>>builder().build();
-
-  /**
-   * Expected general rules for Workgroup ID = 1122.
-   *
-   * <p>Permissions have different case pool ID values.
-   */
-  private static final ImmutableSet<RuleModel> EXPECTED_FIRST_GENERATED_RULES =
-      ImmutableSet.of(
-          RuleModel.builder()
-              .setWorkforceId(1024L)
-              .setWorkgroupId(1122L)
-              .setCasePoolId(2000543L)
-              .setPermissionSetIds(ImmutableSet.of(2048L))
-              .setFilters(emptyFilters)
-              .build(),
-          RuleModel.builder()
-              .setWorkforceId(1024L)
-              .setWorkgroupId(1122L)
-              .setCasePoolId(2000555L)
-              .setPermissionSetIds(ImmutableSet.of(2048L))
-              .setFilters(emptyFilters)
-              .build());
-
-  /**
-   * Expected general rules for Workgroup ID = 2233.
-   *
-   * <p>Permissions have the same case pool ID, but different permission set IDs.
-   */
-  private static final ImmutableSet<RuleModel> EXPECTED_SECOND_GENERATED_RULES =
-      ImmutableSet.of(
-          RuleModel.builder()
-              .setWorkforceId(1024L)
-              .setWorkgroupId(2233L)
-              .setCasePoolId(2000543L)
-              .setPermissionSetIds(ImmutableSet.of(2048L, 2051L))
-              .setFilters(emptyFilters)
-              .build());
->>>>>>> c6e7411... WIP for code review comments.
 
   /** Possible Rule Filters for User 0 - 4. */
   private static final ImmutableList<FilterModel> FILTERS_FOR_USER_0 =
@@ -170,25 +139,6 @@ public class GroupingUtilTest {
   }
 
   @Test
-<<<<<<< HEAD
-=======
-  public void findGeneralRuleForWorkGroupIdTest() throws IOException, CsvException {
-    ImmutableList<UserPoolAssignmentModel> userPoolAssignments =
-        CsvParser.readFromCSVFile(TEST_CSV_FILE_PATH);
-    ImmutableListMultimap<Long, UserPoolAssignmentModel> mapByWorkGroupId =
-        WorkgroupIdGroupingUtil.groupByWorkGroupId(userPoolAssignments);
-
-    ImmutableSet<RuleModel> firstGeneratedRules =
-        WorkgroupIdGroupingUtil.generalRuleByWorkgroupId(mapByWorkGroupId, EXPECTED_FIRST_WORKGROUP_ID);
-    Assert.assertEquals(EXPECTED_FIRST_GENERATED_RULES, firstGeneratedRules);
-
-    ImmutableSet<RuleModel> secondGeneratedRules =
-        WorkgroupIdGroupingUtil.generalRuleByWorkgroupId(mapByWorkGroupId, EXPECTED_SECOND_WORKGROUP_ID);
-    Assert.assertEquals(EXPECTED_SECOND_GENERATED_RULES, secondGeneratedRules);
-  }
-
-  @Test
->>>>>>> c6e7411... WIP for code review comments.
   public void groupByCasePoolIdAndPermissionSetIdTest() throws IOException, CsvException {
     ImmutableList<UserPoolAssignmentModel> userPoolAssignments =
         CsvParser.readFromCSVFile(TEST_CSV_FILE_PATH);
